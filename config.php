d2c8e73edaaabe51d31fdcfda4af59238f289d44
Lito <lito@eordes.com>
2016-07-07 23:02:51 +0200

    Updated example default key

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 return array(
     'Environment' => 'test', // Puedes indicar test o real
     'MerchantCode' => '1234567890',
-    'Key' => 'qwertyasdf0123456789',
+    'Key' => 'sq7HjrUOBfKmC576ILgskD5srU870gJ7',
     'Terminal' => '1',
     'TransactionType' => '0',
     'Currency' => '978',
